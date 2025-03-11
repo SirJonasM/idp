4d935c201f77b6c2933d1209243eaf3d08f4d4ec
SirJonasM <jonas.moewes02@gmail.com>
2025-03-11 23:53:29 +0100

    simple Hello World

--- risc-v-bare-metal-rust-dynamic-memory/src/main.rs
+++ risc-v-bare-metal-rust-dynamic-memory/src/main.rs
@@ -7,20 +7,10 @@ use core::panic::PanicInfo;
 use core::ptr;
 use talc::{ClaimOnOom, Span, Talc, Talck};
 
-#[macro_use]
-extern crate alloc;
-
 global_asm!(include_str!("entry.s"));
 
-static mut ARENA: [u8; 50000] = [0; 50000];
-
-#[global_allocator]
-static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> =
-    Talc::new(unsafe { ClaimOnOom::new(Span::from_array(&mut ARENA)) }).lock();
-
+const UART: *mut u8 = 0x10000000 as *mut u8;
 fn uart_print(message: &str) {
-    const UART: *mut u8 = 0x10000000 as *mut u8;
-
     for c in message.chars() {
         unsafe {
             ptr::write_volatile(UART, c as u8);
@@ -30,15 +20,9 @@ fn uart_print(message: &str) {
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    uart_print("Hello, world!\n");
-
-    let mut ctr = 1;
-
     loop {
-        uart_print(&format!("Ticks: {}\n", ctr));
+        uart_print("Hello, world!\n");
         for _ in 0..5000000 {}
-
-        ctr += 1;
     }
 }
 
