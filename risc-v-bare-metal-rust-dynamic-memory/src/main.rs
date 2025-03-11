c75796a5c95ccec0aee0a4daca9ea885f9ebdd47
SirJonasM <jonas.moewes02@gmail.com>
2025-03-11 23:44:28 +0100

    simple Hello World

--- risc-v-bare-metal-rust-dynamic-memory/src/main.rs
+++ risc-v-bare-metal-rust-dynamic-memory/src/main.rs
@@ -7,16 +7,16 @@ use core::panic::PanicInfo;
 use core::ptr;
 use talc::{ClaimOnOom, Span, Talc, Talck};
 
-#[macro_use]
-extern crate alloc;
+// #[macro_use]
+// extern crate alloc;
 
 global_asm!(include_str!("entry.s"));
 
 static mut ARENA: [u8; 50000] = [0; 50000];
 
-#[global_allocator]
-static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> =
-    Talc::new(unsafe { ClaimOnOom::new(Span::from_array(&mut ARENA)) }).lock();
+// #[global_allocator]
+// static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> =
+//     Talc::new(unsafe { ClaimOnOom::new(Span::from_array(&mut ARENA)) }).lock();
 
 fn uart_print(message: &str) {
     const UART: *mut u8 = 0x10000000 as *mut u8;
@@ -35,8 +35,8 @@ pub extern "C" fn main() -> ! {
     let mut ctr = 1;
 
     loop {
-        let message = format!("Ticks: {}\n", ctr);
-        let temp_str = message.as_str();
+        // let message = format!("Ticks: {}\n", ctr);
+        // let temp_str = message.as_str();
 
         uart_print("Test");
         for _ in 0..5000000 {}
